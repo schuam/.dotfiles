ae173c4b54e00b4d8337887a832536da01751b1d
Andreas Schuster <git@schuam.de>
2021-05-18 16:15:44 +0200

    Adjust floating window config for GPG key entry

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -346,7 +346,7 @@ floating_layout = layout.Floating(float_rules=[
     Match(wm_class='splash'),
     Match(wm_class='toolbar'),
     Match(wm_class='gitk' ),
-    Match(title='pinentry'),  # GPG key password entry
+    Match(wm_class='pinentry-gtk-2'),  # GPG key password entry
     Match(wm_class='ssh-askpass'),  # ssh-askpass
     Match(wm_class='clementine' ),
     Match(wm_class='pavucontrol' ),
