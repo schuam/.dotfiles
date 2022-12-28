a075ecb17491a4e705a08208be3bbb2c8119c1d4
Andreas Schuster <git@schuam.de>
2022-12-28 15:44:51 +0100

    Adjust nvim-tree configuration

--- .config/nvim/lua/user/plugins/nvim-tree.lua
+++ .config/nvim/lua/user/plugins/nvim-tree.lua
@@ -8,22 +8,8 @@ end
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- change color for arrows in tree to light blue
-vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
-
 -- configure nvim-tree
 nvimtree.setup({
-  -- change folder arrow icons
-  renderer = {
-    icons = {
-      glyphs = {
-        folder = {
-          arrow_closed = "", -- arrow when folder is closed
-          arrow_open = "", -- arrow when folder is open
-        },
-      },
-    },
-  },
   on_attach = on_attach,
   remove_keymaps = ({"-"}),
   -- disable window_picker for
