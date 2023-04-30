753328ead2b6c629df0856dfc5cbe129a6c0b018
Andreas Schuster <git@schuam.de>
2023-04-30 12:38:39 +0200

    [nvim] Add function on_attach to nvim-tree setup
    Nvim-tree deprecated some old functionality. An explanation was given on
    how to make Nvim-tree "future proof".

--- .config/nvim/lua/user/plugins/nvim-tree.lua
+++ .config/nvim/lua/user/plugins/nvim-tree.lua
@@ -4,6 +4,102 @@ if not nvim_tree_status then
   return
 end
 
+-- The function on_attach was generated as described in:
+-- https://github.com/nvim-tree/nvim-tree.lua/wiki/Migrating-To-on_attach
+--
+-- This function has been generated from your
+--   view.mappings.list
+--   view.mappings.custom_only
+--   remove_keymaps
+--
+-- You should add this function to your configuration and set on_attach = on_attach in the nvim-tree setup call.
+--
+-- Although care was taken to ensure correctness and completeness, your review is required.
+--
+-- Please check for the following issues in auto generated content:
+--   "Mappings removed" is as you expect
+--   "Mappings migrated" are correct
+--
+-- Please see https://github.com/nvim-tree/nvim-tree.lua/wiki/Migrating-To-on_attach for assistance in migrating.
+--
+
+local function on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+
+  -- Default mappings. Feel free to modify or remove as you wish.
+  --
+  -- BEGIN_DEFAULT_ON_ATTACH
+  -- The following line replaces the explicit setting of default mappings (s.
+  -- https://github.com/nvim-tree/nvim-tree.lua/wiki/Migrating-To-on_attach)
+  api.config.mappings.default_on_attach(bufnr)
+  -- END_DEFAULT_ON_ATTACH
+
+
+  -- Mappings removed via:
+  --   remove_keymaps
+  --   OR
+  --   view.mappings.list..action = ""
+  --
+  -- The dummy set before del is done for safety, in case a default mapping does not exist.
+  --
+  -- You might tidy things by removing these along with their default mapping.
+  vim.keymap.set('n', '-', '', { buffer = bufnr })
+  vim.keymap.del('n', '-', { buffer = bufnr })
+
+end
+--
+-- This function has been generated from your
+--   view.mappings.list
+--   view.mappings.custom_only
+--   remove_keymaps
+--
+-- You should add this function to your configuration and set on_attach = on_attach in the nvim-tree setup call.
+--
+-- Although care was taken to ensure correctness and completeness, your review is required.
+--
+-- Please check for the following issues in auto generated content:
+--   "Mappings removed" is as you expect
+--   "Mappings migrated" are correct
+--
+-- Please see https://github.com/nvim-tree/nvim-tree.lua/wiki/Migrating-To-on_attach for assistance in migrating.
+--
+
+local function on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+
+  -- Default mappings. Feel free to modify or remove as you wish.
+  --
+  -- BEGIN_DEFAULT_ON_ATTACH
+  -- The following line replaces the explicit setting of default mappings (s.
+  -- https://github.com/nvim-tree/nvim-tree.lua/wiki/Migrating-To-on_attach)
+  api.config.mappings.default_on_attach(bufnr)
+  -- END_DEFAULT_ON_ATTACH
+
+
+  -- Mappings removed via:
+  --   remove_keymaps
+  --   OR
+  --   view.mappings.list..action = ""
+  --
+  -- The dummy set before del is done for safety, in case a default mapping does not exist.
+  --
+  -- You might tidy things by removing these along with their default mapping.
+  vim.keymap.set('n', '-', '', { buffer = bufnr })
+  vim.keymap.del('n', '-', { buffer = bufnr })
+
+end
+
+
 -- recommended settings from nvim-tree documentation
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
