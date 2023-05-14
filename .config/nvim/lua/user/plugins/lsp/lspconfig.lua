533d35c8102ac4b6119456513d08d54b1bd3cb81
Andreas Schuster <git@schuam.de>
2023-05-14 22:32:47 +0200

    [nvim] Make somd key bindings depend on lspsaga
    Some keybindings only worked if lspsage was installed and configured.
    But since I don't use lspsage at the moment, I wanted these key bindings
    to work a little differently.

--- .config/nvim/lua/user/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/user/plugins/lsp/lspconfig.lua
@@ -10,6 +10,10 @@ if not cmp_nvim_lsp_status then
   return
 end
 
+-- try to require lspsage, depending on wheather it is installed the key
+-- bindings change a little bit.
+local saga_status, _ = pcall(require, "lspsaga")
+
 local server_list = require("user.plugins.lsp.lsp-server-list").servers
 
 -- enable keybinds only for when lsp server available
@@ -18,6 +22,7 @@ local on_attach = function(client, bufnr)
   -- keybind options
   local opts = { noremap = true, silent = true, buffer = bufnr }
 
+  if saga_status then
   -- set keybinds
   keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", opts) -- show definition, references
   keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts) -- got to declaration
@@ -31,6 +36,12 @@ local on_attach = function(client, bufnr)
   keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
   keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
   keymap.set("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts) -- see outline on right hand side
+  else
+  keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts) -- got to declaration
+  keymap.set("n", "gd", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts) -- got to declaration
+  keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts) -- go to implementation
+  keymap.set("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts) -- see outline on right hand side
+  end
 end
 
 -- used to enable autocompletion (assign to every lsp server config)
