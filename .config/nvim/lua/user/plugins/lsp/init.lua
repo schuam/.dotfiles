07950d769b6e367721f678ad1795d11efb730427
Andreas Schuster <git@schuam.de>
2023-05-14 22:31:12 +0200

    [nvim] Add line to allow me to load lspsaga
    Right now, I'm not using it. It causes some wired screen flickering
    which I don't like and don't want to deal with at the moment. But once I
    find the time to research what causes it, I might actually add lspsaga.

--- .config/nvim/lua/user/plugins/lsp/init.lua
+++ .config/nvim/lua/user/plugins/lsp/init.lua
@@ -1,4 +1,5 @@
 require("user.plugins.lsp.mason")
 require("user.plugins.lsp.mason-lspconfig")
 require("user.plugins.lsp.handlers").setup()
+-- require("user.plugins.lsp.lspsaga")
 require("user.plugins.lsp.lspconfig")
