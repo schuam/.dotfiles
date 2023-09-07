1aed06850d1409eb7b2a06366208bcd5ff08f4a8
Andreas Schuster <git@schuam.de>
2023-09-07 16:06:23 +0200

    [nvim] Add yamlls settings (to set schema files)

---
+++ .config/nvim/lua/user/plugins/lsp/ls-settings/yamlls.lua
@@ -0,0 +1,12 @@
+local opts = {
+  settings = {
+    yaml = {
+      shemas = {
+        ["$XDG_CONFIG_HOME/nvim/lua/user/plugins/lsp/schemas/ansible.json"] = "/*.yml"
+      }
+    }
+  }
+}
+
+return opts
+
